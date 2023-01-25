bccc26bcb2947a6e67916cc7ea45c3334bfb2a0e
Heitor Brandolin <heitorbrandolin@hotmail.com>
2023-01-25 15:56:47 -0300

    Starting to implement Board and Piece

---
+++ src/boardgame/Board.java
@@ -0,0 +1,33 @@
+package boardgame;
+
+public class Board {
+
+	private int rows;
+	private int columns;
+	private Piece[][] pieces;
+
+	public Board(int rows, int colums) {
+		this.rows = rows;
+		this.columns = colums;
+		pieces = new Piece[rows][columns];
+
+	}
+
+	public int getRows() {
+		return rows;
+	}
+
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
+
+	public int getColumns() {
+		return columns;
+	}
+
+	public void setColumns(int columns) {
+		this.columns = columns;
+	}
+	
+	
+}
