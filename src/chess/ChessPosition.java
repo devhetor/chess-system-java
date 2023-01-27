eab63aa0f50cf4917dd45e5cb86fc5d4ed8a0a10
Heitor <heitorbrandolin@hotmail.com>
2023-01-27 18:56:18 -0300

    Check logic

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -29,7 +29,7 @@ public class ChessPosition {
 	}
 	
 	protected static ChessPosition fromPosition(Position position) {
-		return new ChessPosition((char)('a' - position.getColum()), 8 - position.getRow());
+		return new ChessPosition((char)('a' + position.getColum()), 8 - position.getRow());
 	}
 	
 	@Override
